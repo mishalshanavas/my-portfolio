b7a2521365c81f6b6c561d278d1ba4b34256316c
Mishal Shanavas <mishalshanavas8@gmail.com>
2025-07-23 20:31:52 +0530

    initial files

---
+++ app/lib/config.ts
@@ -0,0 +1,16 @@
+export const metaData = {
+  baseUrl: "http://localhost:3000/",
+  title: "Mishal Shanavas()",
+  name: "Mishal Shanavas",
+  ogImage: "/opengraph-image.png",
+  description:
+    "my personal portfolio website built with Next.js, TypeScript, and Tailwind CSS. It showcases my projects, skills, and experiences in a modern and responsive design.",
+};
+
+export const socialLinks = {
+  twitter: "https://x.com/mishal_shanavas",
+  github: "https://github.com/1msirius/mishalshanavas",
+  instagram: "https://www.instagram.com/mishal_shanavas/",
+  linkedin: "https://www.linkedin.com/mishalshanavas",
+  email: "mailto:mishalshanavas@yahoo.com",
+};
---
+++ app/projects/project-data.tsx
@@ -0,0 +1,21 @@
+export interface Project {
+  title: string;
+  year: number;
+  description: string;
+  url: string;
+}
+
+export const projects: Project[] = [
+  {
+    title: "Krish AI",
+    year: 2024,
+    description: "AI-powered platform ",
+    url: "https://github.com/mishalshanavas/krish-ai",
+  },
+  {
+    title: "dummy",
+    year: 2023,
+    description: "test project",
+    url: "https://dummy.xyz/",
+  },
+];
