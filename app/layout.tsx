1b4b74ac3e0de8b48f4360ed2ba560b7962949b2
mishalshanavas <mishalshanavas8@gmail.com>
2025-11-24 21:46:32 +0530

    fix: update favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -57,13 +57,9 @@ export const metadata: Metadata = {
     creator: socialLinks.twitter,
  },
   icons: {
-    icon: [
-      { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
-      { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
-    ],
-    apple: "/apple-touch-icon.png",
+    icon: "/favicon.ico",
+    shortcut: "/favicon.ico",
   },
-  manifest: "/site.webmanifest",
 };
 
 export default function RootLayout({
